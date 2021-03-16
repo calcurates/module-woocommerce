5a78744e91f2039f1d7ee8338e3e61c5f8482814
Ildar Jakparov <ijakparov@gmail.com>
2021-03-16 12:37:51 +0000

    feat: wc extend session data
    Add ship_to_different_address checkbox to ec session

--- wc-calcurates.php
+++ wc-calcurates.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:  WooCommerce Calcurates
  */
@@ -21,6 +22,27 @@ class WC_Calcurates
         self::register_rest_routes();
         add_action('init', [__CLASS__, 'init_shipping']);
 
+        // add feature to add to session ship_to_different_address checkbox status on checkout
+        add_action('woocommerce_checkout_update_order_review', [__CLASS__, 'ship_to_different_address_set_session']);
+
+    }
+
+    public static function ship_to_different_address_set_session($data)
+    {
+        $data_array = [];
+
+        if ($data) {
+
+            parse_str($data, $data_array);
+
+            if (array_key_exists('ship_to_different_address', $data_array) && $data_array['ship_to_different_address']) {
+                WC()->session->set('ship_to_different_address', 1);
+            } else {
+                WC()->session->set('ship_to_different_address', 0);
+            }
+        }
+
+        return $data;
     }
 
     public static function activate()
