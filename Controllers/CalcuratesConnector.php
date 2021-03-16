37c579965fd04e871fcfd78061e7ab426dd177f3
Ildar Jakparov <ijakparov@gmail.com>
2021-03-16 09:13:43 +0000

    feat(shipping-method): extend rates request with data
    Add full product attributes to rates request

--- Controllers/CalcuratesConnector.php
+++ Controllers/CalcuratesConnector.php
@@ -117,20 +117,82 @@ class CalcuratesConnector
 
         foreach ($package['contents'] as $cart_product) {
 
-            $products[] = [
+            $product = $cart_product['data'];
+
+            $data = [
                 "quoteItemId" => $cart_product['product_id'], // FIXME rename later to product_id or id
-                "sku" => $cart_product['data']->get_sku() ?: null,
+                "sku" => $product->get_sku() ?: null,
                 "priceWithTax" => $cart_product['line_tax'],
                 "priceWithoutTax" => $cart_product['line_total'],
                 "discountAmount" => 0,
                 "quantity" => $cart_product['quantity'],
+                "weight" => (float) $product->get_weight(),
+                "length" => (float) $product->get_length(),
+                "width" => (float) $product->get_width(),
+                "height" => (float) $product->get_height(),
                 "inventories" => [
                     [
                         "source" => null,
-                        "quantity" => $cart_product['data']->get_stock_quantity(),
+                        "quantity" => $product->get_stock_quantity(),
                     ],
                 ],
+                "attributes" => [
+                    "date_created" => $product->get_date_created() ? $product->get_date_created()->getTimestamp() : null,
+                    "date_modified" => $product->get_date_modified() ? $product->get_date_modified()->getTimestamp() : null,
+                    "status" => $product->get_status(),
+                    "featured" => $product->get_featured(),
+                    "catalog_visibility" => $product->get_catalog_visibility(),
+                    "price" => (float) $product->get_price(),
+                    "regular_price" => (float) $product->get_regular_price(),
+                    "sale_price" => (float) $product->get_sale_price(),
+                    "date_on_sale_from" => $product->get_date_on_sale_from() ? $product->get_date_on_sale_from()->getTimestamp() : null,
+                    "date_on_sale_to" => $product->get_date_on_sale_to() ? $product->get_date_on_sale_to()->getTimestamp() : null,
+                    "total_sales" => $product->get_total_sales(),
+                    "manage_stock" => $product->get_manage_stock(),
+                    "stock_status" => $product->get_stock_status(),
+                    "backorders" => $product->get_backorders(),
+                    "low_stock_amount" => $product->get_low_stock_amount() ?: null,
+                    "sold_individually" => $product->get_sold_individually(),
+                    "purchase_note" => $product->get_purchase_note(),
+                    "virtual" => $product->get_virtual(),
+                    "downloadable" => $product->get_downloadable(),
+                    "categories" => $product->get_category_ids(),
+                    "tags" => $product->get_tag_ids(),
+                ],
             ];
+
+            if ($cart_product['variation_id'] && $product->get_parent_id()) { // variation
+
+                $parent_product = wc_get_product($product->get_parent_id());
+                $wc_product_attrs = $parent_product->get_attributes();
+
+                if (empty($data['attributes']['categories'])) {
+                    $data['attributes']['categories'] = $parent_product->get_category_ids();
+                }
+
+                if (empty($data['attributes']['tags'])) {
+                    $data['attributes']['tags'] = $parent_product->get_tag_ids();
+                }
+
+                foreach ($product->get_variation_attributes(false) as $taxonomy => $terms_slug) {
+
+                    $term_obj = get_term_by('slug', $terms_slug, $taxonomy);
+                    $term_id = $term_obj->term_id;
+                    $data['attributes']['variation'][] = $term_obj->term_id;
+
+                }
+
+            } else {
+                $wc_product_attrs = $product->get_attributes();
+            }
+
+            foreach ($wc_product_attrs as $attr_name => $attr_obj) {
+                if ($attr_obj->is_taxonomy()) {
+                    $data['attributes'][$attr_name] = $attr_obj->get_options();
+                }
+            }
+
+            $products[] = $data;
         }
         return $products;
     }
