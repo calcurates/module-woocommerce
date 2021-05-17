4a66f134410298b0735abdeb9d6243c7be0f1ef5
Gemorroj <wapinet@mail.ru>
2021-05-17 12:53:14 +0300

    refactoring: use $ prefix for jquery vars, use let/const, use `.woocommerce-shipping-totals` context

--- assets/js/calcurates-checkout.js
+++ assets/js/calcurates-checkout.js
@@ -2,34 +2,37 @@ jQuery(document).ready(function () {
     // setup
     setup_shipping();
 
-    jQuery('body').on('updated_checkout updated_cart_totals', function () {
-
+    jQuery(document.body).on('updated_checkout updated_cart_totals', function () {
         // setup
         setup_shipping();
     });
 
     function setup_shipping() {
-        // setup classes
-        jQuery('.calcurates-checkout__shipping-rate-description').each(function () {
-            var that = jQuery(this);
-            var liElem = that.closest('li');
-            liElem.addClass('calcurates-checkout__shipping-rate');
+        const $root = jQuery('.woocommerce-shipping-totals');
 
-            if (that.hasClass('calcurates-checkout__shipping-rate-description_has-error')) {
-                liElem.addClass('calcurates-checkout__shipping-rate_disabled');
-                liElem.find('input').prop('disabled', true);
+        // setup classes
+        $root.find('.calcurates-checkout__shipping-rate-description').each(function () {
+            let $that = jQuery(this);
+            let $liElem = $that.closest('li');
+            $liElem.addClass('calcurates-checkout__shipping-rate');
+
+            if ($that.hasClass('calcurates-checkout__shipping-rate-description_has-error')) {
+                $liElem.addClass('calcurates-checkout__shipping-rate_disabled');
+                $liElem.find('input[name^="shipping_method"]').prop('disabled', true);
             }
 
-            liElem.find('label').addClass('calcurates-checkout__shipping-rate-label');
+            $liElem.find('label').addClass('calcurates-checkout__shipping-rate-label');
         });
 
         // cart option check if available
-        var current_method = jQuery(':input[name^=shipping_method]:checked');
-        if (current_method.is(':disabled')) {
+        let $shippingMethods = $root.find('input[name^="shipping_method"]');
+        let $currentMethod = $shippingMethods.filter(':checked');
+        if ($currentMethod.prop('disabled')) {
             // remove checked
-            current_method.prop('checked', false);
+            $currentMethod.prop('checked', false);
 
-            jQuery('li:not(.calcurates-checkout__shipping-rate_disabled) :input[name^=shipping_method]:first').prop('checked', true).trigger('change');
+            // check first not disabled
+            $shippingMethods.not($currentMethod).first().prop('checked', true).trigger('change');
         }
     }
 });
