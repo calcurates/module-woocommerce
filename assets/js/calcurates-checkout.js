eb1c6d22aca0dd8daebc45b5cc2bf639944ae7d0
Ildar Jakparov <ijakparov@gmail.com>
2021-05-21 04:57:24 +0000

    fix: improve shipping methods styles and structure
    META DATA: SAAS-1465

--- assets/js/calcurates-checkout.js
+++ assets/js/calcurates-checkout.js
@@ -12,21 +12,31 @@ jQuery(document).ready(function () {
 
         // setup classes
         $root.find('.calcurates-checkout__shipping-rate-description').each(function () {
-            let $that = jQuery(this);
-            let $liElem = $that.closest('li');
-            $liElem.addClass('calcurates-checkout__shipping-rate');
+            const $that = jQuery(this);
+            const $liElem = $that.closest('li').addClass('calcurates-checkout__shipping-rate');
+            const $input = $liElem.find('input[name^="shipping_method"]');
+            const $label = $liElem.find('label').addClass('calcurates-checkout__shipping-rate-label');
 
             if ($that.hasClass('calcurates-checkout__shipping-rate-description_has-error')) {
                 $liElem.addClass('calcurates-checkout__shipping-rate_disabled');
-                $liElem.find('input[name^="shipping_method"]').prop('disabled', true);
+                $input.prop('disabled', true);
             }
 
-            $liElem.find('label').addClass('calcurates-checkout__shipping-rate-label');
+            // create layout to wrap input and label with shipping cost
+            const $controlLayout = jQuery('<div/>', {
+                'class': 'calcurates-checkout__shipping-rate-control-layout'
+            }).prependTo($liElem);
+
+            // wrap
+            $label.prependTo($controlLayout);
+            $input.prependTo($controlLayout);
+
         });
 
         // cart option check if available
-        let $shippingMethods = $root.find('input[name^="shipping_method"]');
-        let $currentMethod = $shippingMethods.filter(':checked');
+        const $shippingMethods = $root.find('input[name^="shipping_method"]');
+        const $currentMethod = $shippingMethods.filter(':checked');
+
         if ($currentMethod.prop('disabled')) {
             // remove checked
             $currentMethod.prop('checked', false);
