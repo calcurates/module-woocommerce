de2dffd033a8be56382a70e77b0d6af16013487c
Gemorroj <wapinet@mail.ru>
2021-09-14 15:27:35 +0300

    build: version 1.2.0

--- src/Logger.php
+++ src/Logger.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Calcurates;
 
-use Psr\Log\AbstractLogger;
-
 // Stop direct HTTP access.
 if (!\defined('ABSPATH')) {
     exit;
@@ -14,9 +12,24 @@ if (!\defined('ABSPATH')) {
 if (!\class_exists(Logger::class)) {
     /**
      * WC_Logger wrapper.
+     *
+     * PSR-3
+     * But the WooCommerce infrastructure found the malware in the https://github.com/php-fig/log/blob/1.1.4/Psr/Log/Test/LoggerInterfaceTest.php file
+     * Currently we can't use the PSR interface correctly
+     *
+     * @see SAAS-2486
      */
-    class Logger extends AbstractLogger
+    class Logger
     {
+        public const EMERGENCY = 'emergency';
+        public const ALERT = 'alert';
+        public const CRITICAL = 'critical';
+        public const ERROR = 'error';
+        public const WARNING = 'warning';
+        public const NOTICE = 'notice';
+        public const INFO = 'info';
+        public const DEBUG = 'debug';
+
         /**
          * Default WooCommerce logger.
          *
@@ -52,15 +65,108 @@ if (!\class_exists(Logger::class)) {
         }
 
         /**
-         * {@inheritdoc}
+         * Logs with an arbitrary level.
+         *
+         * @param mixed[] $context
          */
-        public function log($level, $message, array $context = []): void
+        public function log($level, string $message, array $context = []): void
         {
             if ($context) {
                 $message = "\n".'==== '.$message.' ===='."\n".\print_r($context, true).'====END LOG===='."\n\n";
             }
 
             $this->logger->log($level, $message, ['source' => $this->source]);
+        }
+
+        /**
+         * System is unusable.
+         *
+         * @param mixed[] $context
+         */
+        public function emergency(string $message, array $context = []): void
+        {
+            $this->log(self::EMERGENCY, $message, $context);
+        }
+
+        /**
+         * Action must be taken immediately.
+         *
+         * Example: Entire website down, database unavailable, etc. This should
+         * trigger the SMS alerts and wake you up.
+         *
+         * @param mixed[] $context
+         */
+        public function alert(string $message, array $context = []): void
+        {
+            $this->log(self::ALERT, $message, $context);
+        }
+
+        /**
+         * Critical conditions.
+         *
+         * Example: Application component unavailable, unexpected exception.
+         *
+         * @param mixed[] $context
+         */
+        public function critical(string $message, array $context = []): void
+        {
+            $this->log(self::CRITICAL, $message, $context);
+        }
+
+        /**
+         * Runtime errors that do not require immediate action but should typically
+         * be logged and monitored.
+         *
+         * @param mixed[] $context
+         */
+        public function error(string $message, array $context = []): void
+        {
+            $this->log(self::ERROR, $message, $context);
+        }
+
+        /**
+         * Exceptional occurrences that are not errors.
+         *
+         * Example: Use of deprecated APIs, poor use of an API, undesirable things
+         * that are not necessarily wrong.
+         *
+         * @param mixed[] $context
+         */
+        public function warning(string $message, array $context = []): void
+        {
+            $this->log(self::WARNING, $message, $context);
+        }
+
+        /**
+         * Normal but significant events.
+         *
+         * @param mixed[] $context
+         */
+        public function notice(string $message, array $context = []): void
+        {
+            $this->log(self::NOTICE, $message, $context);
+        }
+
+        /**
+         * Interesting events.
+         *
+         * Example: User logs in, SQL logs.
+         *
+         * @param mixed[] $context
+         */
+        public function info(string $message, array $context = []): void
+        {
+            $this->log(self::INFO, $message, $context);
+        }
+
+        /**
+         * Detailed debug information.
+         *
+         * @param mixed[] $context
+         */
+        public function debug(string $message, array $context = []): void
+        {
+            $this->log(self::DEBUG, $message, $context);
         }
     }
 }
