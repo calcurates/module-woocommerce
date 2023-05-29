1c2983f253a772370fddfd5af18eb80c9ad045c6
ijakparov <is5169@yandex.ru>
2023-05-29 16:32:53 +0500

    fix carriers error visibility [SAAS-4298]

--- src/Rates/Extractors/RateShoppingRatesExtractor.php
+++ src/Rates/Extractors/RateShoppingRatesExtractor.php
@@ -16,48 +16,26 @@ class RateShoppingRatesExtractor extends RatesExtractorAbstract
         $ready_rates = [];
 
         foreach ($data as $rate_shopping) {
-            if (!$rate_shopping['success']) {
-                if ($rate_shopping['message']) {
-                    $ready_rates[] = [
-                        'has_error' => true,
-                        'id' => $rate_shopping['id'],
-                        'label' => $this->resolveLabel($rate_shopping),
-                        'cost' => 0,
-                        'tax' => 0,
-                        'message' => $rate_shopping['message'],
-                        'delivery_date_from' => null,
-                        'delivery_date_to' => null,
-                        'priority' => $rate_shopping['priority'],
-                        'priority_item' => null,
-                        'rate_image' => $rate_shopping['imageUri'],
-                    ];
-                }
-                continue;
-            }
 
             foreach ($rate_shopping['carriers'] as $carrier) {
-                if (!$carrier['success']) {
-                    continue;
-                }
-                foreach ($carrier['rates'] as $rate) {
-                    if (!$rate['success']) {
-                        continue;
-                    }
 
+                foreach ($carrier['rates'] as $rate) {
                     $services_names = [];
                     $services_messages = [];
                     $services_ids = [];
                     $services_priority = null;
 
-                    foreach ($rate['services'] as $service) {
-                        if ($service['message']) {
-                            $services_messages[] = $service['message'];
-                        }
+                    if ($rate['success']) {
+                        foreach ($rate['services'] as $service) {
+                            if ($service['message']) {
+                                $services_messages[] = $service['message'];
+                            }
 
-                        $services_ids[] = $service['id'];
-                        $services_names[] = $this->resolveLabel($service);
-                        if (null !== $service['priority']) {
-                            $services_priority += $service['priority'];
+                            $services_ids[] = $service['id'];
+                            $services_names[] = $this->resolveLabel($service);
+                            if (null !== $service['priority']) {
+                                $services_priority += $service['priority'];
+                            }
                         }
                     }
 
@@ -66,12 +44,12 @@ class RateShoppingRatesExtractor extends RatesExtractorAbstract
                     $services_names = \implode(', ', \array_unique($services_names));
 
                     $ready_rates[] = [
-                        'has_error' => false,
+                        'has_error' => !$rate['success'],
                         'id' => $rate_shopping['id'].'_'.$carrier['id'].'_'.$services_ids,
                         'label' => $this->resolveLabel($carrier).'. '.$services_names,
                         'cost' => $rate['rate']['cost'] ?? 0,
                         'tax' => $rate['rate']['tax'] ?? 0,
-                        'message' => $rate_shopping['message'].' '.$services_messages,
+                        'message' => $rate['success'] ? $rate_shopping['message'].' '.$services_messages : $rate['message'],
                         'delivery_date_from' => isset($rate['rate']['estimatedDeliveryDate']) ? $rate['rate']['estimatedDeliveryDate']['from'] : null,
                         'delivery_date_to' => isset($rate['rate']['estimatedDeliveryDate']) ? $rate['rate']['estimatedDeliveryDate']['to'] : null,
                         'priority' => $rate_shopping['priority'],
