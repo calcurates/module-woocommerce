662290297e1d255376c46588329ca630d09f0698
Gemorroj <wapinet@mail.ru>
2024-05-31 15:09:12 +0300

    build: upd deps

--- assets/lib/air-datepicker/locale/fi.js
+++ assets/lib/air-datepicker/locale/fi.js
@@ -1,12 +1,12 @@
 var DATEPICKER_LANG = {
-  days: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'],
-  daysShort: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
-  daysMin: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
-  months: ['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'],
-  monthsShort: ['Tammi', 'Helmi', 'Maalis', 'Huhti', 'Touko', 'Kesä', 'Heinä', 'Elo', 'Syys', 'Loka', 'Marras', 'Joulu'],
-  today: 'Tänään',
-  clear: 'Tyhjennä',
-  dateFormat: 'dd.MM.yyyy',
-  timeFormat: 'HH:mm',
-  firstDay: 1
+    days: ['Sunnuntai', 'Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'],
+    daysShort: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
+    daysMin: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
+    months: ['Tammikuu','Helmikuu','Maaliskuu','Huhtikuu','Toukokuu','Kesäkuu', 'Heinäkuu','Elokuu','Syyskuu','Lokakuu','Marraskuu','Joulukuu'],
+    monthsShort: ['Tammi', 'Helmi', 'Maalis', 'Huhti', 'Touko', 'Kesä', 'Heinä', 'Elo', 'Syys', 'Loka', 'Marras', 'Joulu'],
+    today: 'Tänään',
+    clear: 'Tyhjennä',
+    dateFormat: 'dd.MM.yyyy',
+    timeFormat: 'HH:mm',
+    firstDay: 1
 };
