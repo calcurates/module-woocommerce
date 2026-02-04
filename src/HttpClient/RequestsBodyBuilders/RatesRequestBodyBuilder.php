d2cf3aee8927a683c56dab869bb91d1401b31a6d
Gemorroj <pavel.dyakonov@amasty.com>
2026-02-04 15:58:18 +0300

    fix: php Warning: Undefined array key *

--- src/HttpClient/RequestsBodyBuilders/RatesRequestBodyBuilder.php
+++ src/HttpClient/RequestsBodyBuilders/RatesRequestBodyBuilder.php
@@ -128,16 +128,16 @@ class RatesRequestBodyBuilder
         } else {
             $customer_session_data = \WC()->session->get('customer', []);
 
-            $postcode = $customer_session_data['shipping_postcode'] ?? $customer_session_data['postcode'];
-            $first_name = $customer_session_data['shipping_first_name'] ?? $customer_session_data['first_name'];
-            $last_name = $customer_session_data['shipping_last_name'] ?? $customer_session_data['last_name'];
-            $company = $customer_session_data['shipping_company'] ?? $customer_session_data['company'];
-            $phone = $customer_session_data['shipping_phone'] ?? $customer_session_data['phone'];
-            $state = $customer_session_data['shipping_state'] ?? $customer_session_data['state'];
-            $city = $customer_session_data['shipping_city'] ?? $customer_session_data['city'];
-            $addr_1 = $customer_session_data['shipping_address_1'] ?? $customer_session_data['address_1'];
-            $addr_2 = $customer_session_data['shipping_address_2'] ?? $customer_session_data['address_2'];
-            $country_code = $customer_session_data['shipping_country'] ?? $customer_session_data['country'];
+            $postcode = $customer_session_data['shipping_postcode'] ?? $customer_session_data['postcode'] ?? null;
+            $first_name = $customer_session_data['shipping_first_name'] ?? $customer_session_data['first_name'] ?? null;
+            $last_name = $customer_session_data['shipping_last_name'] ?? $customer_session_data['last_name'] ?? null;
+            $company = $customer_session_data['shipping_company'] ?? $customer_session_data['company'] ?? null;
+            $phone = $customer_session_data['shipping_phone'] ?? $customer_session_data['phone'] ?? null;
+            $state = $customer_session_data['shipping_state'] ?? $customer_session_data['state'] ?? null;
+            $city = $customer_session_data['shipping_city'] ?? $customer_session_data['city'] ?? null;
+            $addr_1 = $customer_session_data['shipping_address_1'] ?? $customer_session_data['address_1'] ?? null;
+            $addr_2 = $customer_session_data['shipping_address_2'] ?? $customer_session_data['address_2'] ?? null;
+            $country_code = $customer_session_data['shipping_country'] ?? $customer_session_data['country'] ?? null;
         }
 
         if ($first_name || $last_name) {
